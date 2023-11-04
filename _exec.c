d131305f0f2bb8fa70538b7a46380507c30080c0
Mwangi-Jack <jasonmuturi2000@gmail.com>
2023-11-04 15:17:46 +0300

    fixed inbuilt exit

--- _exec.c
+++ _exec.c
@@ -13,31 +13,41 @@
 void _exec(char *comm)
 {
 	pid_t child_pid = fork();
+	char *_exit = "exit";
 
-	if (child_pid == -1)
+	if (strcmp(_exit, comm) == 0)
 	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	} else if (child_pid == 0)
-	{
-		char *args[120];
-		int count = 0;
-		char *token = strtok(comm, " ");
-
-		while (token != NULL)
-		{
-			args[count++] = token;
-			token = strtok(NULL, " ");
-		}
-		args[count] = NULL;
-
-		execvp(args[0], args);
-
-		_print("Error executing command.\n");
-		exit(EXIT_FAILURE);
-
+		exit(0);
 	} else
 	{
-		wait(NULL);
+		if (child_pid == -1)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		} else if (child_pid == 0)
+		{
+			char *args[120];
+			int count = 0;
+			char *token = strtok(comm, " ");
+
+			while (token != NULL)
+			{
+				args[count++] = token;
+				token = strtok(NULL, " ");
+			}
+			args[count] = NULL;
+
+			execvp(args[0], args);
+
+			_print("command not found.\n");
+			exit(EXIT_FAILURE);
+
+		} else
+		{
+			wait(NULL);
+		}
+
 	}
+
+
 }
