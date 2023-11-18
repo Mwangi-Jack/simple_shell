929c65c0f1c55bfb30f555c7f3d173cf61b01eba
eluemnaomi <eluemnaomi4@gmail.com>
2023-11-17 20:13:04 -0800

    first commit

--- shell.h
+++ shell.h
@@ -1,18 +1,91 @@
 #ifndef __SHELL_H__
 #define __SHELL_H__
 
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <string.h>
-#include <stdlib.h>
 
+#define MAX_ARGS 1024
+#define BUFSIZ 1024
+#define BUFFER_SIZE 1024
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <limits.h>
+#include <string.h>
+
+extern char **environ;
+
+/**
+ * struct list_s - singly linked list
+ * @str: string data in the node
+ * @len: length of the string
+ * @next: points to the next node in the list
+ *
+ * Description: A structure for creating singly linked lists.
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+void cleanup(void);
+void handle_sigint(int signo);
+
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+void *safe_realloc(void *ptr, size_t old_size, size_t new_size);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int _fgetc(FILE *stream);
+
+int execute_external_command(char *command, char **arguments);
+void print_error(const char *program_name, const char *message);
+int wordcount(char *s, char *delimiter);
+char **strtow(char *str, char *delimiter);
+int _atoi(char *s);
+void change_directory(char **arguments);
+void exit_shell(char **arguments);
+void execute_command(char *command_string);
+int handle_builtin(char *command, char **arguments, char *command_string);
+int execute_absolute_path(char *command, char **arguments);
+
+char *remove_extra_spaces(const char *input);
+int is_whitespace(const char ch);
+void free_list(list_t *head);
+list_t *add_node(list_t **head, const char *str);
+list_t *linked_path(char *name);
+void find_command_in_path(char *command, char **arguments);
+char *construct_path(char *directory, char *command);
+int is_executable(const char *path);
+void nonInteractiveMode(FILE *script);
+void print_path_directories(const char *path);
+void print_environment(void);
+void print_prompt(void);
+char *_getenv(const char *name);
+
+char *_strdup(const char *str);
+int _putchar(char c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 void disp_prompt(void);
 void _print(const char *str);
 void _read_input(char *comm, size_t size);
 void _exec(char *comm);
 char *_strcat(char *dest, char *src);
-
 #endif
